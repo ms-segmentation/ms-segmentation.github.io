a21e89027d7b64135b80b4a9875be1f31f9eacc9
rob3rtroessler <sherrytangssr@gmail.com>
2022-12-15 08:47:55 +0000

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,153 @@
+let imageNames = ["m_3807502_se_18_1_naip-new_cp10", "m_3807502_se_18_1_naip-new_cp1", "m_3807502_se_18_1_naip-new_cp2", "m_3807502_se_18_1_naip-new_cp3", "m_3807502_se_18_1_naip-new_cp4", "m_3807502_se_18_1_naip-new_cp5", "m_3807502_se_18_1_naip-new_cp6", "m_3807502_se_18_1_naip-new_cp7", "m_3807502_se_18_1_naip-new_cp8", "m_3807502_se_18_1_naip-new_cp9", "m_3807541_sw_18_1_naip-new_cp10", "m_3807541_sw_18_1_naip-new_cp1", "m_3807541_sw_18_1_naip-new_cp2", "m_3807541_sw_18_1_naip-new_cp3", "m_3807541_sw_18_1_naip-new_cp4", "m_3807541_sw_18_1_naip-new_cp5", "m_3807541_sw_18_1_naip-new_cp6", "m_3807541_sw_18_1_naip-new_cp7", "m_3807541_sw_18_1_naip-new_cp8", "m_3807541_sw_18_1_naip-new_cp9", "m_3807552_se_18_1_naip-new_cp10", "m_3807552_se_18_1_naip-new_cp1", "m_3807552_se_18_1_naip-new_cp2", "m_3807552_se_18_1_naip-new_cp3", "m_3807552_se_18_1_naip-new_cp4", "m_3807552_se_18_1_naip-new_cp5", "m_3807552_se_18_1_naip-new_cp6", "m_3807552_se_18_1_naip-new_cp7", "m_3807552_se_18_1_naip-new_cp8", "m_3807552_se_18_1_naip-new_cp9", "m_3807557_se_18_1_naip-new_cp10", "m_3807557_se_18_1_naip-new_cp1", "m_3807557_se_18_1_naip-new_cp2", "m_3807557_se_18_1_naip-new_cp3", "m_3807557_se_18_1_naip-new_cp4", "m_3807557_se_18_1_naip-new_cp5", "m_3807557_se_18_1_naip-new_cp6", "m_3807557_se_18_1_naip-new_cp7", "m_3807557_se_18_1_naip-new_cp8", "m_3807557_se_18_1_naip-new_cp9", "m_3807608_nw_18_1_naip-new_cp10", "m_3807608_nw_18_1_naip-new_cp1", "m_3807608_nw_18_1_naip-new_cp2", "m_3807608_nw_18_1_naip-new_cp3", "m_3807608_nw_18_1_naip-new_cp4", "m_3807608_nw_18_1_naip-new_cp5", "m_3807608_nw_18_1_naip-new_cp6", "m_3807608_nw_18_1_naip-new_cp7", "m_3807608_nw_18_1_naip-new_cp8", "m_3807608_nw_18_1_naip-new_cp9", "m_3807622_nw_18_1_naip-new_cp10", "m_3807622_nw_18_1_naip-new_cp1", "m_3807622_nw_18_1_naip-new_cp2", "m_3807622_nw_18_1_naip-new_cp3", "m_3807622_nw_18_1_naip-new_cp4", "m_3807622_nw_18_1_naip-new_cp5", "m_3807622_nw_18_1_naip-new_cp6", "m_3807622_nw_18_1_naip-new_cp7", "m_3807622_nw_18_1_naip-new_cp8", "m_3807622_nw_18_1_naip-new_cp9", "m_3807639_nw_18_1_naip-new_cp10", "m_3807639_nw_18_1_naip-new_cp1", "m_3807639_nw_18_1_naip-new_cp2", "m_3807639_nw_18_1_naip-new_cp3", "m_3807639_nw_18_1_naip-new_cp4", "m_3807639_nw_18_1_naip-new_cp5", "m_3807639_nw_18_1_naip-new_cp6", "m_3807639_nw_18_1_naip-new_cp7", "m_3807639_nw_18_1_naip-new_cp8", "m_3807639_nw_18_1_naip-new_cp9", "m_3807646_ne_18_1_naip-new_cp10", "m_3807646_ne_18_1_naip-new_cp1", "m_3807646_ne_18_1_naip-new_cp2", "m_3807646_ne_18_1_naip-new_cp3", "m_3807646_ne_18_1_naip-new_cp4", "m_3807646_ne_18_1_naip-new_cp5", "m_3807646_ne_18_1_naip-new_cp6", "m_3807646_ne_18_1_naip-new_cp7", "m_3807646_ne_18_1_naip-new_cp8", "m_3807646_ne_18_1_naip-new_cp9", "m_3807664_se_18_1_naip-new_cp10", "m_3807664_se_18_1_naip-new_cp1", "m_3807664_se_18_1_naip-new_cp2", "m_3807664_se_18_1_naip-new_cp3", "m_3807664_se_18_1_naip-new_cp4", "m_3807664_se_18_1_naip-new_cp5", "m_3807664_se_18_1_naip-new_cp6", "m_3807664_se_18_1_naip-new_cp7", "m_3807664_se_18_1_naip-new_cp8", "m_3807664_se_18_1_naip-new_cp9"]
+// , "m_3907533_sw_18_1_naip-new_cp10", "m_3907533_sw_18_1_naip-new_cp1", "m_3907533_sw_18_1_naip-new_cp2", "m_3907533_sw_18_1_naip-new_cp3", "m_3907533_sw_18_1_naip-new_cp4", "m_3907533_sw_18_1_naip-new_cp5", "m_3907533_sw_18_1_naip-new_cp6", "m_3907533_sw_18_1_naip-new_cp7", "m_3907533_sw_18_1_naip-new_cp8", "m_3907533_sw_18_1_naip-new_cp9", "m_3907628_sw_18_1_naip-new_cp10", "m_3907628_sw_18_1_naip-new_cp1", "m_3907628_sw_18_1_naip-new_cp2", "m_3907628_sw_18_1_naip-new_cp3", "m_3907628_sw_18_1_naip-new_cp4", "m_3907628_sw_18_1_naip-new_cp5", "m_3907628_sw_18_1_naip-new_cp6", "m_3907628_sw_18_1_naip-new_cp7", "m_3907628_sw_18_1_naip-new_cp8", "m_3907628_sw_18_1_naip-new_cp9", "m_3907659_nw_18_1_naip-new_cp10", "m_3907659_nw_18_1_naip-new_cp1", "m_3907659_nw_18_1_naip-new_cp2", "m_3907659_nw_18_1_naip-new_cp3", "m_3907659_nw_18_1_naip-new_cp4", "m_3907659_nw_18_1_naip-new_cp5", "m_3907659_nw_18_1_naip-new_cp6", "m_3907659_nw_18_1_naip-new_cp7", "m_3907659_nw_18_1_naip-new_cp8", "m_3907659_nw_18_1_naip-new_cp9", "m_3907719_ne_18_1_naip-new_cp10", "m_3907719_ne_18_1_naip-new_cp1", "m_3907719_ne_18_1_naip-new_cp2", "m_3907719_ne_18_1_naip-new_cp3", "m_3907719_ne_18_1_naip-new_cp4", "m_3907719_ne_18_1_naip-new_cp5", "m_3907719_ne_18_1_naip-new_cp6", "m_3907719_ne_18_1_naip-new_cp7", "m_3907719_ne_18_1_naip-new_cp8", "m_3907719_ne_18_1_naip-new_cp9", "m_3907739_ne_18_1_naip-new_cp10", "m_3907739_ne_18_1_naip-new_cp1", "m_3907739_ne_18_1_naip-new_cp2", "m_3907739_ne_18_1_naip-new_cp3", "m_3907739_ne_18_1_naip-new_cp4", "m_3907739_ne_18_1_naip-new_cp5", "m_3907739_ne_18_1_naip-new_cp6", "m_3907739_ne_18_1_naip-new_cp7", "m_3907739_ne_18_1_naip-new_cp8", "m_3907739_ne_18_1_naip-new_cp9", "m_3907739_nw_18_1_naip-new_cp10", "m_3907739_nw_18_1_naip-new_cp1", "m_3907739_nw_18_1_naip-new_cp2", "m_3907739_nw_18_1_naip-new_cp3", "m_3907739_nw_18_1_naip-new_cp4", "m_3907739_nw_18_1_naip-new_cp5", "m_3907739_nw_18_1_naip-new_cp6", "m_3907739_nw_18_1_naip-new_cp7", "m_3907739_nw_18_1_naip-new_cp8", "m_3907739_nw_18_1_naip-new_cp9", "m_3907747_ne_18_1_naip-new_cp10", "m_3907747_ne_18_1_naip-new_cp1", "m_3907747_ne_18_1_naip-new_cp2", "m_3907747_ne_18_1_naip-new_cp3", "m_3907747_ne_18_1_naip-new_cp4", "m_3907747_ne_18_1_naip-new_cp5", "m_3907747_ne_18_1_naip-new_cp6", "m_3907747_ne_18_1_naip-new_cp7", "m_3907747_ne_18_1_naip-new_cp8", "m_3907747_ne_18_1_naip-new_cp9", "m_3907763_ne_18_1_naip-new_cp10", "m_3907763_ne_18_1_naip-new_cp1", "m_3907763_ne_18_1_naip-new_cp2", "m_3907763_ne_18_1_naip-new_cp3", "m_3907763_ne_18_1_naip-new_cp4", "m_3907763_ne_18_1_naip-new_cp5", "m_3907763_ne_18_1_naip-new_cp6", "m_3907763_ne_18_1_naip-new_cp7", "m_3907763_ne_18_1_naip-new_cp8", "m_3907763_ne_18_1_naip-new_cp9", "m_3907822_nw_17_1_naip-new_cp10", "m_3907822_nw_17_1_naip-new_cp1", "m_3907822_nw_17_1_naip-new_cp2", "m_3907822_nw_17_1_naip-new_cp3", "m_3907822_nw_17_1_naip-new_cp4", "m_3907822_nw_17_1_naip-new_cp5", "m_3907822_nw_17_1_naip-new_cp6", "m_3907822_nw_17_1_naip-new_cp7", "m_3907822_nw_17_1_naip-new_cp8", "m_3907822_nw_17_1_naip-new_cp9", "m_3907827_se_17_1_naip-new_cp10", "m_3907827_se_17_1_naip-new_cp1", "m_3907827_se_17_1_naip-new_cp2", "m_3907827_se_17_1_naip-new_cp3", "m_3907827_se_17_1_naip-new_cp4", "m_3907827_se_17_1_naip-new_cp5", "m_3907827_se_17_1_naip-new_cp6", "m_3907827_se_17_1_naip-new_cp7", "m_3907827_se_17_1_naip-new_cp8", "m_3907827_se_17_1_naip-new_cp9", "m_3907937_sw_17_1_naip-new_cp10", "m_3907937_sw_17_1_naip-new_cp1", "m_3907937_sw_17_1_naip-new_cp2", "m_3907937_sw_17_1_naip-new_cp3", "m_3907937_sw_17_1_naip-new_cp4", "m_3907937_sw_17_1_naip-new_cp5", "m_3907937_sw_17_1_naip-new_cp6", "m_3907937_sw_17_1_naip-new_cp7", "m_3907937_sw_17_1_naip-new_cp8", "m_3907937_sw_17_1_naip-new_cp9"]
+let selectedImageName = 'm_3807502_se_18_1_naip-new_cp10';
+
+function populateTable(data) {
+  let scoreTableHTML = `<div class='swipe-wrap'>`;
+
+  // populate string
+  data.forEach((selectedImageName) => {
+      console.log(selectedImageName)
+
+      scoreTableHTML += `
+      <div><b>   <table class="table-score">
+	  <tbody>
+		  <tr>
+			  <td>Real Image</td>
+			  <td>Ground Truth</td>
+			  <td>Model 100% Real</td>
+			  <td>Model 25% Synthetic</td>
+			  <td>Model 50% Synthetic</td>
+			  <td>Model 75% Synthetic</td>
+			  <td>Model 100% Synthetic</td>
+			  <td>Model 200% Synthetic</td>
+		  </tr>
+		  <tr>
+			  <td><img class="align-self-center"
+				  src="data/final_res/original_images_real/${selectedImageName}.png"
+				  style="width: 100%"></td>
+			  <td></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_000_checkpoint_epoch5_mix_0_real.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_025_checkpoint_epoch5_mix_25_real.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_050_checkpoint_epoch5_mix_50_real.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_075_checkpoint_epoch5_mix_75_real.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_100_checkpoint_epoch5_mix_100_real.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_200_checkpoint_epoch5_mix_100_real.png"
+				  style="width: 100%"></td>
+		  </tr>
+		  <tr>
+			  <td></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/ground_truth_masks/${selectedImageName}.png"
+				  style="width: 100%"></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+		  </tr>
+		  <tr>
+			  <td><img class="align-self-center"
+				  src="data/final_res/original_images_syn/${selectedImageName}.png"
+				  style="width: 100%"></td>
+			  <td></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_000_checkpoint_epoch5_mix_0_syn.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_025_checkpoint_epoch5_mix_25_syn.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_050_checkpoint_epoch5_mix_50_syn.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_075_checkpoint_epoch5_mix_75_syn.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_100_checkpoint_epoch5_mix_100_syn.png"
+				  style="width: 100%"></td>
+			  <td><img class="align-self-center"
+				  src="data/final_res/${selectedImageName}/checkpoints_syn_200_checkpoint_epoch5_mix_100_syn.png"
+				  style="width: 100%"></td>
+		  </tr>
+		  <tr>
+			  <td>Synthetic Image</td>
+			  <td> </td>
+			  <td> </td>
+			  <td> </td>
+			  <td> </td>
+			  <td> </td>
+			  <td> </td>
+			  <td> </td>
+		  </tr>
+	  </tbody>
+  </table></b></div>
+`
+
+  })
+  scoreTableHTML += `</div>`;
+  // update string
+  document.getElementById("mySwipe").innerHTML = scoreTableHTML;
+
+}
+$(document).ready(function () {
+	// Smooth navigation scroll
+	$(".navLink").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 1200, function () {
+				window.location.hash = hash;
+			});
+		}
+	});
+	populateTable(imageNames);
+	// pure JS
+	var elem = document.getElementById('mySwipe');
+	window.mySwipe = Swipe(elem, {
+			  // startSlide: 4,
+			  // auto: 3000,
+			  // continuous: true,
+			  // disableScroll: true,
+			  // stopPropagation: true,
+			  // callback: function(index, element) {},
+			  // transitionEnd: function(index, element) {}
+	});
+			
+});
+
+// section fade in during scroll
+$(document).on("scroll", function () {
+	//Calculate how far down the page we are
+	var pageTop = $(document).scrollTop();
+	//Bottom of browser
+	var pageBottom = pageTop + $(window).height();
+
+	var tags = $("section");
+	//loop through tags
+	for (var i = 0; i < tags.length; i++) {
+		var tag = tags[i]
+		var y = $(tag).position()
+		// if tag is further up thank the bottom of window
+		if (y.top + 300 < pageBottom) {
+			//add visible class to make it fade in
+			$(tag).addClass("visible");
+		} else {
+			$(tag).removeClass("visible")
+		}
+	}
+});
